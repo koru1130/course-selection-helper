460090f28df71dc9acb2cb33e1c0ea4361b6c6b1
koru1130 <me@koru.me>
2021-08-30 20:28:29 +0800

    fix coursebar swapping by adding key

--- src/components/TimeTableD/GridCell.svelte
+++ src/components/TimeTableD/GridCell.svelte
@@ -12,7 +12,7 @@ export let height: number
 
 
 <div style="position: relative; min-height: {height}em;">
-    {#each v as x, i}
+    {#each v as x, i (x)}
         {#if x != "PlaceHolder"}
             <CourseBar serNo={x[0]} sectionId={x[1]} nth={i} />
         {/if}
