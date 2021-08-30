33906a891d8dc44c1078dbcecad4e544e6eea98d
koru1130 <me@koru.me>
2021-08-30 08:06:14 +0800

    change test data

--- src/stores.ts
+++ src/stores.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import type { CourseList } from './types';
 
-const sampleCandidateCourses = ['74987', '12179', '34256']
+const sampleCandidateCourses = ['74987', '12179', '34256', '80120', '12941', '69889']
 
 export let candidateCourses  = writable<CourseList>([]) //writable<CourseList>([]);
 export let selectedCourses   = writable<CourseList>(sampleCandidateCourses);
