6b9d2f4136702f38101ba27e5046399f981fd0eb
koru1130 <me@koru.me>
2022-08-19 04:34:23 +0800

    update sample

--- src/stores.ts
+++ src/stores.ts
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 import type { CourseDisplayStatus, CourseList } from './types';
 import { persistentWritable, writableFamily } from './utils';
 
-const sampleCandidateCourses = ["12941","76953","69889","80120","54547"]
+const sampleCandidateCourses = ['58935', '58523', '97120']
 
 export let candidateCourses  = persistentWritable<CourseList>("candidateCourses", []) //writable<CourseList>([]);
 export let selectedCourses   = persistentWritable<CourseList>("selectedCourses", sampleCandidateCourses);
